a55b64c7a2af02216ad7405624a1bdf84c47e642
frederick-bruce <frederick.bruce.dev@gmail.com>
2021-11-17 13:20:49 -0500

    file/folder setup

---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./whatGPT3.css"
+
+const WhatGPT3 = () => {
+    return (
+        <div>
+            WhatGp3
+        </div>
+    )
+}
+
+export default WhatGPT3
