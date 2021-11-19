c620830faf20012993e357a0d832d44bacd1350b
frederick-bruce <frederick.bruce.dev@gmail.com>
2021-11-19 01:26:46 -0500

    header section finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Footer, Blog, Header, Possibility, WhatGPT3, Features } from "./containers";
 import { CTA, Brand, Navbar } from "./components";
+import "./App.css";
 
 const App = () => {
     return (
